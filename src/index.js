f747b875fe46a5d073d9416a89c0d638d586417e
Raga <ragawilliam570@gmail.com>
2022-09-05 15:15:38 +0300

    Add index.js file with initializing code for todo list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = <h1>Hello from Create React App</h1>;
+console.log(element);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(element, document.getElementById('root'));
