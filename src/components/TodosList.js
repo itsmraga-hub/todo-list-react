d4a47b83e3360f68d98a8a7561608a6cefc14e63
Raga <ragawilliam570@gmail.com>
2022-09-12 14:24:09 +0300

    Add Todolists component - a ul conatining multiple todo items

---
+++ src/components/TodosList.js
@@ -0,0 +1,22 @@
+import React, { PureComponent } from 'react';
+import TodoItem from './TodoItem';
+
+class TodosList extends PureComponent {
+  render() {
+    const { todos, handleChangeProps, deleteTodoProps } = this.props;
+    return (
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={handleChangeProps}
+            deleteTodoProps={deleteTodoProps}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodosList;
